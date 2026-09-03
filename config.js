126b624c2fd9dc87dc6ca917122d9dfd4923c996
alexischan <alexreyd44@gmail.com>
2026-09-03 02:16:52 -0500

    Change usePairingCode to true in config.js

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const config = {
 
   session: {
     pairingNumber: "+380970121849", // Número de WA que será emparejado
-    usePairingCode: false, // true = Código de Emparejamiento, false = Código QR
+    usePairingCode: true, // true = Código de Emparejamiento, false = Código QR
   },
 
   bot: {
